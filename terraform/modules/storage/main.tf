10818767a75bc9b00dc0d085b055fdf87e6fbe55
Khronossu <purinboonpetch@gmail.com>
2026-04-13 13:35:12 +0700

    fix: add filter block to S3 lifecycle rule

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -40,6 +40,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "mods" {
     id     = "expire-old-versions"
     status = "Enabled"
 
+    filter {}
+
     noncurrent_version_expiration {
       noncurrent_days = 30
     }
